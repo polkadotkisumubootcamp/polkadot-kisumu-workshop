a3092fb5c4f32ced43a830d27e88601f634ba227
quochieng <aiyanarich64@gmail.com>
2025-06-05 21:53:26 +0300

    feat:my first rust project

---
+++ quinter/hello_world/target/debug/deps/hello_world-0ff18d27cf616650.d
@@ -0,0 +1,5 @@
+/home/quinterabok/polkadot/polkadot-kisumu-workshop/quinter/hello_world/target/debug/deps/libhello_world-0ff18d27cf616650.rmeta: src/main.rs
+
+/home/quinterabok/polkadot/polkadot-kisumu-workshop/quinter/hello_world/target/debug/deps/hello_world-0ff18d27cf616650.d: src/main.rs
+
+src/main.rs:
---
+++ quinter/hello_world/target/debug/deps/hello_world-2443d5adfb8dea4e.d
@@ -0,0 +1,5 @@
+/home/quinterabok/polkadot/polkadot-kisumu-workshop/quinter/hello_world/target/debug/deps/hello_world-2443d5adfb8dea4e: src/main.rs
+
+/home/quinterabok/polkadot/polkadot-kisumu-workshop/quinter/hello_world/target/debug/deps/hello_world-2443d5adfb8dea4e.d: src/main.rs
+
+src/main.rs:
