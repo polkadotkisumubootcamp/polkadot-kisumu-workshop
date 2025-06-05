4dc56bb747352082c265c2108e7897a7fa3247d4
vinolia-E <vinolia.b.19@gmail.com>
2025-06-05 22:06:37 +0300

    feat: First Polkadot bootcamp project, printing Hello Kisumu

---
+++ vinolia_hello/target/debug/deps/vinolia_hello.d
@@ -0,0 +1,5 @@
+C:\Users\lenovo\Desktop\polkadot_bootcamp\polkadot-kisumu-workshop\vinolia_hello\target\debug\deps\vinolia_hello.exe: src\main.rs
+
+C:\Users\lenovo\Desktop\polkadot_bootcamp\polkadot-kisumu-workshop\vinolia_hello\target\debug\deps\vinolia_hello.d: src\main.rs
+
+src\main.rs:
