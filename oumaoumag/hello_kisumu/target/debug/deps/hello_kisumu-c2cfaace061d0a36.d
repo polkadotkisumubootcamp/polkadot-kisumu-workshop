df0764b49bf99db2f23ffeb0ed4ccf15763d7634
oumaoumag <ouma.godwin10@gmail.com>
2025-06-05 21:15:41 +0300

    Initialize hello_kisumu project with Cargo configuration and main source file

---
+++ oumaoumag/hello_kisumu/target/debug/deps/hello_kisumu-c2cfaace061d0a36.d
@@ -0,0 +1,5 @@
+/home/oumouma/web3-stuff/polkadot-kisumu-workshop/oumaoumag/hello_kisumu/target/debug/deps/libhello_kisumu-c2cfaace061d0a36.rmeta: src/main.rs
+
+/home/oumouma/web3-stuff/polkadot-kisumu-workshop/oumaoumag/hello_kisumu/target/debug/deps/hello_kisumu-c2cfaace061d0a36.d: src/main.rs
+
+src/main.rs:
