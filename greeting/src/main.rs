c408db26586b8eb1331969c6741e75df96308d52
vinodhiambo <vincent@berrijam.com>
2025-06-07 17:17:54 +0300

    initial commit

--- greeting/src/main.rs
+++ greeting/src/main.rs
@@ -1,7 +1,9 @@
+// greetings fn
 fn greet(team: &str) -> String {
     format!("Hello, Team {}!", team)
 }
 
+// main fn
 fn main() {
     let message = greet("Kisumu Builders");
     println!("{}", message);
