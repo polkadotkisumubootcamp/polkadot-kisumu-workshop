e70a74b12adb376a82a1e917e0352fe2f5824b3a
Malika7188 <asmanodeny@gmail.com>
2025-06-07 20:32:08 +0300

    feat: added greetings function

---
+++ greetings/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let message = "Hello Polkadot Kisumu!"; 
+
+    println!("{}", message);
+}
---
+++ greetings/target/debug/deps/greetings-af0316d07fe51c53.d
@@ -0,0 +1,5 @@
+/home/malika/polkadot-kisumu-workshop/greetings/target/debug/deps/libgreetings-af0316d07fe51c53.rmeta: src/main.rs
+
+/home/malika/polkadot-kisumu-workshop/greetings/target/debug/deps/greetings-af0316d07fe51c53.d: src/main.rs
+
+src/main.rs:
