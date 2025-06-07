ad310c51c2f19fc86f312e3ae4f8b544022d7ee2
Vinolia-E <vinolia.b.19@gmail.com>
2025-06-07 22:05:32 +0300

    Class 2: work with mutable variables, shadowing of variables

---
+++ vinolia_mutable/src/main.rs
@@ -0,0 +1,38 @@
+const MAX_SPEED : u32 = 100;
+const APP_VERSION : &str = "version 2.0";
+
+fn main() {
+
+   let mut city = "Hello Kisumu!";
+   city = "hello Kisii";
+   let age : i32 = 50;
+   let pi : f64 = 3.14;
+   let is_in_class : bool = true;
+   let grade : char = 'A';
+   let name : &str = "John Doe";
+
+   let sum = 5 + 3;
+   let diff = 5 -3;
+   let mult = 5 *3;
+   let div = 5 / 3;
+   let mod = 5 % 3;
+
+
+//    Shadowing - creating a new variable under same name
+
+let x = 5;
+let x = x + 2;
+
+let spaces = "         ";
+let spaces = spaces.len(); // shadow it as integer
+  
+  
+   println!("{city} This is a polkadot bootcamp");
+   println!("The maximun speed allowed is : {} Km/hr", MAX_SPEED);
+   println!("The new realeased application is : {},", APP_VERSION);
+   println!(" Name: {}, Age: {}, Pi: {}, In class: {}, Grade: {}",name, age,pi,is_in_class,grade);
+   println!("Shadow value of x is: {}", x);
+   println!("Number of spaces is : {}", spaces);
+   println!("{},{},{},{},{}", sum, diff, mult, div, mod);
+
+}
---
+++ vinolia_mutable/target/debug/deps/vinolia_mutable-297846054535cc70.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/lenovo/Desktop/polkadot_bootcamp/polkadot-kisumu-workshop/vinolia_mutable/target/debug/deps/vinolia_mutable-297846054535cc70: src/main.rs
+
+/mnt/c/Users/lenovo/Desktop/polkadot_bootcamp/polkadot-kisumu-workshop/vinolia_mutable/target/debug/deps/vinolia_mutable-297846054535cc70.d: src/main.rs
+
+src/main.rs:
