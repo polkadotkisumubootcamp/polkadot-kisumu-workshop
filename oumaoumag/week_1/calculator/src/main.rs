36dfece8be6b42500de0d9f753189b172a380a68
oumaoumag <ouma.godwin10@gmail.com>
2025-06-09 23:35:36 +0300

    Update main.rs to enhance user interaction with a custom greeting and prompt for input

--- oumaoumag/week_1/calculator/src/main.rs
+++ oumaoumag/week_1/calculator/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Welcome To Ouma's Custom Simple Math Calculator!");
+
+    println!("Please input your simple math problem.");
+
+    let mut problem = String::new();
+
+    io::stdin()
+    .read_line(&mut problem)
+    .expect("Failed to read line");
+
+    println!("You types in: {}", problem)
 }
