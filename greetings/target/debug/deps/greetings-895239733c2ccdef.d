4114392d019d39cf165b873d11203f6da7de3cb2
Malika7188 <asmanodeny@gmail.com>
2025-06-09 23:35:15 +0300

    files moved to gitignore

--- greetings/target/debug/deps/greetings-895239733c2ccdef.d
+++
@@ -1,5 +0,0 @@
-/home/malika/polkadot-kisumu-workshop/greetings/target/debug/deps/libgreetings-895239733c2ccdef.rmeta: src/main.rs
-
-/home/malika/polkadot-kisumu-workshop/greetings/target/debug/deps/greetings-895239733c2ccdef.d: src/main.rs
-
-src/main.rs:
