e573302b246379589da2a332edaff582aa363236
oumaoumag <ouma.godwin10@gmail.com>
2025-06-10 00:48:42 +0300

    Fix token parsing and error handling in calculator function

--- oumaoumag/week_1/calculator/src/lib.rs
+++ oumaoumag/week_1/calculator/src/lib.rs
@@ -3,7 +3,7 @@
 pub fn calculator(expression: &str) {
     let tokens: Vec<&str> = expression.split_whitespace().collect();
 
-    if tokens.ekn() != 3 {
+    if tokens.len() != 3 {
         eprinln!("Usage: <number> <op> <number>, e.g 1 + 2");
         return
     };
@@ -11,13 +11,20 @@ pub fn calculator(expression: &str) {
 
     let 1st = match tokens[0].parse::<f64>() {
         Ok(v) => v,
-        ERr(_) => {
+        Err(_) => {
             eprinln!("Invalid number: {}", tokens[0]);
             return;
         }
     };
-    let _sign = input[1] as &str;
-    let _2nd = input[2] as f64;
+
+    let 2nd = tokens[2].parse::<f64>() {
+        Ok(v) => v,
+        Err(_) => {
+            eprinln!("Invalid number: {}", tokens[2]);
+            return
+        }
+    },
+    let sign = tokens[1].parse::< &str;
 
     for token  in input {
         println!("{}", token);
