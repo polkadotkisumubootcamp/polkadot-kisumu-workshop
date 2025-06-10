7b2b02f8f5cd55feb264ffddaa853e7f351ad475
vandisi <vinolia.b.19@gmail.com>
2025-06-10 18:13:23 +0300

    feat: Assign each value of the command line

---
+++ week_one_assignment/target/debug/deps/week_one_assignment-65e9824fc3870a1c.d
@@ -0,0 +1,5 @@
+/home/vandisi/Desktop/github-projects/polkadot-kisumu-workshop/week_one_assignment/target/debug/deps/week_one_assignment-65e9824fc3870a1c: src/main.rs
+
+/home/vandisi/Desktop/github-projects/polkadot-kisumu-workshop/week_one_assignment/target/debug/deps/week_one_assignment-65e9824fc3870a1c.d: src/main.rs
+
+src/main.rs:
