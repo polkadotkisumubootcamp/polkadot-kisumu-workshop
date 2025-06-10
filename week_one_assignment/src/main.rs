158f493808de5038f8e5320c4ec021782bb904bf
vandisi <vinolia.b.19@gmail.com>
2025-06-10 17:57:43 +0300

    feat: Introduce calculator function

--- week_one_assignment/src/main.rs
+++ week_one_assignment/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
     println!("Hello, world!");
+
+}
+
+fn calculator(a: i32, b: i32, op : &str ) -> i32 {
+
+    if op == "+" {
+        return a + b;
+    } else if op == "-" {
+        return a - b;
+    } else if op == "*" {
+        return a * b;
+    } else if op == "/" {
+        if b != 0 {
+            return a / b;
+        } else {
+            panic!("Division by zero is not allowed.");
+        }
+    } else if op == "%" {
+        if b != 0 {
+            return a % b;
+        } else {
+            panic!("Division by zero is not allowed.");
+        }
+        } else {
+        panic!("Unsupported operation: {}", op);
+    }   
 }
