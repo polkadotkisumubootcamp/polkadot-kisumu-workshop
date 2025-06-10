dab49c1bb7676744f3c838b01829eb157b9ddede
oumaoumag <ouma.godwin10@gmail.com>
2025-06-10 17:16:26 +0300

    Add color codes

--- oumaoumag/week_1/calculator/src/main.rs
+++ oumaoumag/week_1/calculator/src/main.rs
@@ -2,20 +2,25 @@ use std::io;
 use calculator::*;
 
 fn main() {
+    // Display welcome message
     println!("{}",
     colorize(&format!("Hello, Welcome To Ouma's Custom Simple Math Calculator!"), 21)
 );
 
+    // Prompt user for input
     println!("{}",
     colorize(&format!("Please input your simple math problem."), 4)
 );
 
+    // Create a mutable string to store user input
     let mut problem = String::new();
 
+    // Read user input from stdin
     io::stdin()
     .read_line(&mut problem)
     .expect(&colorize(&format!("Failed to read line"), 196)
 );
 
-     calculator(problem.trim());
+    // Process the math problem after trimming whitespace
+    calculator(problem.trim());
 }
