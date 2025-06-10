64f54189b869cb034fa6e26e97a6039bad88d5d8
Vinolia-E <vinolia.b.19@gmail.com>
2025-06-10 21:53:57 +0300

    feat: Call the calculator function

--- week_one_assignment/src/main.rs
+++ week_one_assignment/src/main.rs
@@ -4,19 +4,26 @@ fn main() {
    
     // let mut args = std::env::args();
     let args: Vec<String> = env::args().collect();
+    //Confirms that we have three arguements. The first arguement(at index zero), is the program name, so it is not parsed.
     if args.len() != 4 {
-        println!("Usage: <num1> <operation> <num2>");
+        println!("Usage: <num1> <operator> <num2>");
         return;
     }
 
-    // Parse the command line arguments
-    mut value1: i32 = args[1].parse().expect("Please provide a valid integer for num1.");
-    let operation: &str = &args[2];
-    mut value2: i32 = args[3].parse().expect("Please provide a valid integer for num2.");
+    // Parse the command line arguments from the second arguement, which is at index zero.
+    // The first arguement is the first value, and should be an integer
+    // The second arguement should be an operator. Always ensure that the multiplication "*" sign is inside quotation marks to avoid missinterpretation by cli
+    // The third arguement should be the second value, which must also be a number. 
+   let value1: i32 = args[1].parse().expect("Please provide a valid integer for num1.");
+    let operator: &str = &args[2];
+    let value2: i32 = args[3].parse().expect("Please provide a valid integer for num2.");
+    println!("{:?}", args);
 
+    let result = calculator(value1, value2, operator);
+    println!("The result is: {}", result) //result
 }
 
-// The calculator function takes two integers and a string representing an operation.
+// The calculator function takes two integers and a string representing an operator.
 fn calculator(a: i32, b: i32, op : &str ) -> i32 {
 
     if op == "+" {
@@ -38,6 +45,6 @@ fn calculator(a: i32, b: i32, op : &str ) -> i32 {
             panic!("Division by zero is not allowed.");
         }
         } else {
-        panic!("Unsupported operation: {}", op);
+        panic!("Unsupported operator: {}", op);
     }   
 }
