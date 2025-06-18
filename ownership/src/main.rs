fd43831bf5b9aa99bee269d33edb7716273d9b59
vandisi <vinolia.b.19@gmail.com>
2025-06-18 17:29:12 +0300

    feat: Add the second way of handling ownership, using immutable refference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -4,4 +4,14 @@ fn main() {
 let x = String::from("bootcamp");
 let y = x.clone();  // clone() creates a deep copy of x
 println!("x: {}, y: {}", x, y);
+
+// way 2: Immutable borrow
+
+let mut s = String::from("Quinter");
+print_length(&mut s);
+println!("Second way: {}", s);
+}
+
+fn print_length(s : &mut String) {
+    s.push_str(" is amazing at rust!");
 }
