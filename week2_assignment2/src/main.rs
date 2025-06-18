fb169e233c4fb6615ffc15c9905680d1d24a8bc8
vandisi <vinolia.b.19@gmail.com>
2025-06-18 18:19:22 +0300

    feat: Include the question in main.rs

--- week2_assignment2/src/main.rs
+++ week2_assignment2/src/main.rs
@@ -1,3 +1,19 @@
+// Build a tiny “Guest Tracker” app (console-based)
+
+// Ask for guest names and store them (practice
+// ownership).
+
+// Let parts of your code borrow guest names to
+// greet them (practice borrowing).
+
+// Ensure only one part edits guest details at a
+// time (mutable borrow).
+
+// Add a note (comment) in code about where
+// scope ends and memory is freed.
+
+// Add a function that returns a borrowed guest
+// name (practice lifetimes).
 fn main() {
     println!("Hello, world!");
 }
