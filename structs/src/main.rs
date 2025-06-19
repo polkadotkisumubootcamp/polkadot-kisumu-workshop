85875b2d87228bcffaac14dc13bdf78ca3960fe6
vinolia-E <vinolia.b.19@gmail.com>
2025-06-19 22:14:07 +0300

    feat: Add and instanse of implement (impl) while handling struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,50 +1,71 @@
-fn main() {
-//     struct Developer {
-//         name: String,
-//         language: String,
-//         experience: u8,
-//     }
+// fn main() {
+// //     struct Developer {
+// //         name: String,
+// //         language: String,
+// //         experience: u8,
+// //     }
    
-// let dev: Developer = Developer{
-//    name: String::from("Cheryl"),
-//    language: String::from("Rust"),
-//    experience: 5,
+// // let dev: Developer = Developer{
+// //    name: String::from("Cheryl"),
+// //    language: String::from("Rust"),
+// //    experience: 5,
 
-// };
-// println!("{} code in {}  with {} years of experience", dev.name, dev.language, dev.experience)
+// // };
+// // println!("{} code in {}  with {} years of experience", dev.name, dev.language, dev.experience)
 
-// struct Developer2 {
-//     name: String,
-//     language: String,
-//     experience: u8,
+// // struct Developer2 {
+// //     name: String,
+// //     language: String,
+// //     experience: u8,
+// // }
+
+// // //  Helper function that creates instance of Developer
+// // fn create_developer(name: &str, language: &str, experience:)
+
+// struct Designer{
+//     name:String,
+//     tool: String,
 // }
 
-// //  Helper function that creates instance of Developer
-// fn create_developer(name: &str, language: &str, experience:)
+// struct Developer{
+//     name: String,
+//     language: String,
+// }
 
-struct Designer{
-    name:String,
-    tool: String,
-}
+// let dev: Developer = Developer{
+//     name: String::from("Hassan"),
+//     language: String::from("Rust"),
+// };
 
-struct Developer{
-    name: String,
-    language: String,
-}
+// let designer: Designer = Designer {
+//     name: String::from("Bob"),
+//     tool: String::from("Figma"),
+// };
 
-let dev: Developer = Developer{
-    name: String::from("Hassan"),
-    language: String::from("Rust"),
-};
-
-let designer: Designer = Designer {
-    name: String::from("Bob"),
-    tool: String::from("Figma"),
-};
-
-println!("{} codes in {} language", dev.name, dev.language);
-println!("{} designs using {} tool", designer.name, designer.tool);
+// println!("{} codes in {} language", dev.name, dev.language);
+// println!("{} designs using {} tool", designer.name, designer.tool);
 
 
+// }
+
+// Implementation:
+
+fn main() {
+    struct Developer{
+        name: String,
+        experience: u8,
+    }
+
+    impl Developer {
+        fn summary(&self) -> String {
+            format!("{} has {} years of experience", self.name, self.experience)
+
+        }
+    }
+    let dev: Developer = Developer {
+        name:String::from("Bob"),
+        experience: 5,
+    };
+    println!("{}", dev.summary())
 }
 
