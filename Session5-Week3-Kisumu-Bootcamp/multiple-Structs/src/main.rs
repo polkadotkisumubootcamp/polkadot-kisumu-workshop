a5cfcf387515a913a0be89a071987b31506a7377
Githaiga22 <allangithaiga5@gmail.com>
2025-06-19 16:30:54 +0300

    Add example program demonstrating multiple structs in Rust

---
+++ Session5-Week3-Kisumu-Bootcamp/multiple-Structs/src/main.rs
@@ -0,0 +1,23 @@
+// This program demonstrates the use of multiple structs in Rust.
+// It defines two structs: Developer and Designer, each with their own fields.
+// fn main() {
+    
+//     struct Developer {
+//         name:String,
+//         language: String,
+//     }
+//     struct Designer {
+//         name: String,
+//         tool: String,
+//     }
+//     let dev = Developer {
+//         name:String::from("Hassan"),
+//         language:String::from("Rust"),
+//     };
+//     let designer = Designer {
+//         name:String::from("Ivy"),
+//         tool:String::from("Figma"),
+//     };
+//     println!("{} codes in {} language",dev.name, dev.language);
+//     println!("{} designs  using {} tool", designer.name, designer.tool);
+// }
