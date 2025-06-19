e71be63ff1395e763fef571cd7095c57a699a1e1
vandisi <vinolia.b.19@gmail.com>
2025-06-19 17:26:53 +0300

    Add decoration to the app

--- week2_assignment2/src/main.rs
+++ week2_assignment2/src/main.rs
@@ -16,18 +16,38 @@
 // name (practice lifetimes).
 
 use std::io;
+use colored::*;
 fn main() {
-   println!("\n\t\t🌟🌟Welcome to guest tracker🤝🎉\n");
-   println!("Kindly input your name below 👇👇");
+      println!(
+        "\n\t\t{}",
+        "🌟🌟Welcome to guest tracker🤝🎉\n".yellow().bold()
+    );
+   println!("{}", "Kindly input your first name below 👇👇".truecolor(154, 205, 50));
     let mut guest_name = String::new();
 
     io::stdin().read_line(&mut guest_name).expect("Failed to read input");
     let guest_name = guest_name.trim(); // Remove any trailing newline or spaces
-    // println!("\nHello, {}! Welcome to our guest tracker app!", guest_name);
     greet_guest(guest_name);
+
+    println!("{}","\n Kindly input your second name below 👇👇\n".truecolor(154, 205, 50));
+
+    let mut second_name = String::new();
+    io::stdin().read_line(&mut second_name).expect("Failed to read input");
+    let second_name = second_name.trim(); // Remove any trailing newline or spaces
+
+    full_name(&mut guest_name.to_string(), &second_name.to_string());
 
 }
 
 fn greet_guest(guest: &str) {
     println!("\nHello, {}! Welcome to our guest tracker app 🤝 \nIt's great to see you here! 🎉\n", guest);
+}
+
+fn full_name(s_name: &mut String, sec_name: &String) {
+    s_name.push_str(" ");
+    s_name.push_str(sec_name);
+    // The scope of s_name ends here, and memory will be freed when it goes out
+    // of scope, which is at the end of this function.
+    // sec_name is borrowed and will not be freed until it goes out of scope in the main function.
+    println!("\nYour full guest name is: {}", s_name);
 }
