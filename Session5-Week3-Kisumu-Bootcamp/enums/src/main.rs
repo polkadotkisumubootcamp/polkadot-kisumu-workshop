21a66e9721a8c9923dd6be31fc822957a120d2f6
Githaiga22 <allangithaiga5@gmail.com>
2025-06-21 19:28:06 +0300

    Add example of enum usage in Rust with mood states

---
+++ Session5-Week3-Kisumu-Bootcamp/enums/src/main.rs
@@ -0,0 +1,20 @@
+// //1. Define the enum:
+// enum Mood {
+//     Happy,
+//     Angry,
+//     Sleepy,
+// }
+
+
+// fn main() {
+// //2. Use the enum:
+//     let my_mood = Mood::Sleepy; // This is how you create an instance of the Mood enum
+//    // 3. Check what it is (match it):
+
+//     match my_mood {
+//         Mood::Happy => println!("Yay! Let's play!"), // if it's happy, print this
+//         Mood::Angry => println!("Grrr... leave me alone."),  // if it's angry, print this
+//         Mood::Sleepy => println!("Zzz... need nap."), // if it's sleepy, print this
+//     } 
+    
+// }
