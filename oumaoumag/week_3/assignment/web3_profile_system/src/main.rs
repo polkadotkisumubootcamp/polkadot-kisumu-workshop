4c0e9d95785febaeb2f5fbda86ba406518287199
oumaoumag <ouma.godwin10@gmail.com>
2025-06-22 19:09:51 +0300

    feat: Add Developer & Designer structs each have fields that describe tthe professional's attribute

---
+++ oumaoumag/week_3/assignment/web3_profile_system/src/main.rs
@@ -0,0 +1,20 @@
+
+// Developer struct
+struct Developer {
+    name: String,
+    favourite_lang: String,
+    blockchain_protocal: String,
+    years_experince: u8,
+}
+
+// Designer struct
+struct Designer {
+    name: String, 
+    favourite_tool: String,
+    favourite_blockchain: String,
+    years_experince: u8,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
