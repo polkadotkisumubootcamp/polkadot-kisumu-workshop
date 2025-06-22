b2a06951db07f5aaa374696f788a1ea077ca760c
doonyango <datieno001@gmail.com>
2025-06-22 10:16:58 +0300

    feat: implement introduce() method for developer

--- week3_session6/src/main.rs
+++ week3_session6/src/main.rs
@@ -11,3 +11,13 @@ struct Developer {
     favorite_lang: String,
     years_experience: u8,
 }
+
+// Implementing the Introduce trait for Developer
+impl Introduce for Developer {
+    fn introduced(&self) {
+        println!(
+            "Hi, I'm {}, a Web3 Developer who loves building on {} with {} years of experience!",
+            self.name, self.favorite_lang, self.years_experience
+        );
+    }
+}
