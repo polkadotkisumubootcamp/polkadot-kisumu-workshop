2bbe01e8ac63780658ffd880f70a724e8413b595
CynthiaOketch <cynthiaoketch6@gmail.com>
2025-06-23 18:28:11 +0300

    implemented the Introduce trait for Developer

--- week3/web3_profile/src/main.rs
+++ week3/web3_profile/src/main.rs
@@ -9,3 +9,12 @@ struct Developer<'a> {
     years_experience: u8,
 }
 
+// Implement the Introduce trait for Developer
+impl<'a> Introduce for Developer<'a> {
+    fn introduce(&self) {
+        println!(
+            "Hi, I'm {}. I love writing {} and have {} years of experience in Web3.",
+            self.name, self.favorite_lang, self.years_experience
+        );
+    }
+}
