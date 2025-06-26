cff13e3fbfd8ef610660b73b431708dea4217221
CynthiaOketch <cynthiaoketch6@gmail.com>
2025-06-26 15:24:43 +0300

    feat : created and populated the contributors vector

---
+++ week4/session7/developer_dashboard/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+      // Create a vector of 6 contributor names
+    let mut contributors: Vec<String> = vec![
+        "Cynthia".to_string(),
+        "Brian".to_string(),
+        "Aisha".to_string(),
+        "Kelvin".to_string(),
+        "Grace".to_string(),
+        "Liam".to_string(),
+    ];
+}
