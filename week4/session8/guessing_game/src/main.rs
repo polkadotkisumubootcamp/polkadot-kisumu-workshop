32862a73b39f3d443829082ae0838b626716e72e
CynthiaOketch <cynthiaoketch6@gmail.com>
2025-07-01 00:34:43 +0300

    feat(time): display current time at game start

--- week4/session8/guessing_game/src/main.rs
+++ week4/session8/guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use rand::Rng;
+use std::io;
+use colored::*;
+use chrono::Local;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", "Welcome to the Rust Guessing Game!".bold().cyan());
+
+    let now = Local::now();
+    println!("{}", format!("Current time: {}", now.format("%Y-%m-%d %H:%M:%S")).dimmed());
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+
+
 }
